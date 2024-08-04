e8bbdf645069a03ba640cdccfa793db51c5d636e
Reza Zahedi <r.zahedi@gmail.com>
2024-08-04 11:50:35 -0700

    Profile loading state

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -18,33 +18,41 @@ export default function Profile({
 }) {
 
   const [profile, setProfile] = useState<Profile | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
  
   // TODO: Fetch profile details using Spotify API call locally
   
   // Spotify API endpoint for profile
   const url = "https://api.spotify.com/v1/me"
 
-  fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    }
 
-  }).then(async (res) => {
-    const json = await res.json()
-    setProfile({
-      name: json.display_name,
-      href: json.href,
-      id: json.id,
-      image: json.images[1].url,
-      followers: json.followers.total,
-    });
-    console.log( 'profile:', profile );
-  })
+  useEffect(() => {
+    setLoading(true)
+
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+  
+    }).then(async (res) => {
+      const json = await res.json()
+      setLoading(false)
+
+      setProfile({
+        name: json.display_name,
+        href: json.href,
+        id: json.id,
+        image: json.images[1].url,
+        followers: json.followers.total,
+      });
+    })
+  }, [token])
 
   return (
     <div>
-      {profile !== null && 
+      {loading && <p>Loading...</p>}
+      {!loading && profile &&
         <>
           <p>{profile.name}</p>
           <Image src={profile.image} alt={profile.name} width={100} height={100} />
