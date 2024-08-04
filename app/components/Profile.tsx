6019f933c413a8abe0a6a758d143149396ee0a88
Reza Zahedi <r.zahedi@gmail.com>
2024-08-04 11:44:07 -0700

    Request and show profile details

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -1,11 +1,24 @@
 "use client"
 
+import { useEffect, useState } from "react"
+import Image from "next/image"
+
+type Profile = {
+  name: string,
+  href: string,
+  id: string,
+  image: string,
+  followers: number,
+}
+
 export default function Profile({
   token
 }: {
   token: string
 }) {
-  
+
+  const [profile, setProfile] = useState<Profile | null>(null)
+ 
   // TODO: Fetch profile details using Spotify API call locally
   
   // Spotify API endpoint for profile
@@ -18,13 +31,25 @@ export default function Profile({
     }
 
   }).then(async (res) => {
-    console.log( 'profile:', await res.json() );
+    const json = await res.json()
+    setProfile({
+      name: json.display_name,
+      href: json.href,
+      id: json.id,
+      image: json.images[1].url,
+      followers: json.followers.total,
+    });
+    console.log( 'profile:', profile );
   })
 
   return (
     <div>
-      {token}
-      <p>Profile image and name</p>
+      {profile !== null && 
+        <>
+          <p>{profile.name}</p>
+          <Image src={profile.image} alt={profile.name} width={100} height={100} />
+        </>
+      }
       {process.env.SPOTIFY_CLIENT_ID}
     </div>
   )
