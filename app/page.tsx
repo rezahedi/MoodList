9b4a1b97cbbaea9f562cb240b6d756a971e27f89
Reza Zahedi <r.zahedi@gmail.com>
2024-08-04 10:41:00 -0700

    Get access token in server

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import MoodMeter from "./components/MoodMeter";
 import Profile from "./components/Profile";
 
-export default function Home(
+export default async function Home(
   {
     searchParams
   }: {
@@ -14,6 +14,26 @@ export default function Home(
   const scope = 'user-read-private user-read-email';
 
   if(searchParams.code) {
+    console.log( 'searchParams.code:', searchParams.code );
+    
+    // Get access token server-side
+    await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic OTU5Yjg2ZGE5YjgzNDJjYTg5MDkzYTFjYzQ3Mjc3ODU6NTFhMzhkMTdjMWMyNDdlYmJjNWNjYTJhZjdiNGU3OGU='
+      }, 
+      body: 
+        'grant_type=authorization_code' +
+        '&code=' + searchParams.code +
+        '&redirect_uri=' + process.env.NEXT_PUBLIC_BASE_URL
+    }).then(async (res) => {
+      console.log( 'res:', await res.json() );
+    }).catch(err => {
+      console.log( 'err:', err );
+    });
+    
+
     return (
       <main>
         <p>Connected!</p>
@@ -26,7 +46,7 @@ export default function Home(
 
   return (
     <main>
-      <Link href={`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=${scope}&redirect_uri=${process.env.NEXT_PUBLIC_BASE_URL}`}>
+      <Link href={`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.SPOTIFY_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_BASE_URL}`}>
         Connect to Spotify
       </Link>
     </main>
