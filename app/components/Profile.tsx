4bece9df232f1b50d77bf576a4167d5375966b36
Reza Zahedi <r.zahedi@gmail.com>
2024-08-04 11:22:55 -0700

    Request profile details

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -7,11 +7,25 @@ export default function Profile({
 }) {
   
   // TODO: Fetch profile details using Spotify API call locally
+  
+  // Spotify API endpoint for profile
+  const url = "https://api.spotify.com/v1/me"
+
+  fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+
+  }).then(async (res) => {
+    console.log( 'profile:', await res.json() );
+  })
 
   return (
     <div>
-      <p>Profile image and name</p>
       {token}
+      <p>Profile image and name</p>
+      {process.env.SPOTIFY_CLIENT_ID}
     </div>
   )
 }
