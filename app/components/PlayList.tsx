d283a6988da118bb9b59086d230f85cce25c8dae
Reza Zahedi <r.zahedi@gmail.com>
2024-08-05 13:05:00 -0700

    Add more track details

--- app/components/PlayList.tsx
+++ app/components/PlayList.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function PlayList({
   tracks
@@ -9,8 +10,20 @@ export default function PlayList({
     <div className='space-y-2'>
       PlayList:
       {tracks && tracks.map(track => 
-        <div key={track.id} className='border rounded-md p-4'>
-          {track.name}
+        <div
+          key={track.id}
+          className='border rounded-md p-4 flex gap-2 items-center'
+        >
+          <Image
+            src={track.album.image} alt={track.album.name}
+            width={100} height={100}
+            className=''
+          />
+          <div className='flex flex-col'>
+            <a href={track.url}><b>{track.name}</b></a>
+            <a href="#">Artist Name</a>
+            <a href={track.album.url}>{track.album.name}</a>
+          </div>
         </div>
       )}
     </div>
