798e0fb8d18ecb62c93d4e98f302742359cda993
Reza Zahedi <r.zahedi@gmail.com>
2024-08-05 17:06:41 -0700

    Create save playlist button

--- app/components/PlayList.tsx
+++ app/components/PlayList.tsx
@@ -6,26 +6,34 @@ export default function PlayList({
 }: {
   tracks: Track[]
 }) {
+  const savePlaylist = () => {
+    console.log('save clicked!')
+  }
+
   return (
     <div className='space-y-2'>
-      PlayList:
-      {tracks && tracks.map(track => 
-        <div
-          key={track.id}
-          className='border rounded-md p-4 flex gap-2 items-center'
-        >
-          <Image
-            src={track.album.image} alt={track.album.name}
-            width={100} height={100}
-            className=''
-          />
-          <div className='flex flex-col'>
-            <a href={track.url}><b>{track.name}</b></a>
-            <a href="#">Artist Name</a>
-            <a href={track.album.url}>{track.album.name}</a>
+      {tracks &&
+      <>
+        PlayList:
+        {tracks.map(track => 
+          <div
+            key={track.id}
+            className='border rounded-md p-4 flex gap-2 items-center'
+          >
+            <Image
+              src={track.album.image} alt={track.album.name}
+              width={100} height={100}
+            />
+            <div className='flex flex-col'>
+              <a href={track.url} target='_blank'><b>{track.name}</b></a>
+              <a href="#" target='_blank'>Artist Name</a>
+              <a href={track.album.url} target='_blank'>{track.album.name}</a>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+        <p>Do you want to save these as a playlist in your Spotify account?</p>
+        <button className='rounded-md bg-orange-700 px-4 py-2' onClick={savePlaylist}>Yes, Save it!</button>
+      </>}
     </div>
   )
 }
